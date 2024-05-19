a863c5d2be956a003b2f05667d0a839e7a30a1c5
Andrei-Gabriel Paiu-Rotundu <114349768+djxsrouk@users.noreply.github.com>
2024-05-19 01:12:39 +0100

    commit lesson-8

--- react-app/src/components/Tutors/Tutors.jsx
+++ react-app/src/components/Tutors/Tutors.jsx
@@ -2,21 +2,25 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Paper from "../Paper/Paper";
 import styles from "./Tutors.module.css";
-import { nanoid } from "nanoid";
+import FormTutors from "../FormTutors/FormTutors";
+import ThemedButton from "../ThemedButton/ThemedButton";
 
 const Tutors = ({ tutors }) => {
   const [showForm, setShowForm] = useState(false);
   const [tutorsData, setTutorsData] = useState(tutors);
 
+  const handleRemoveTutor = (id) => {
+    setTutorsData(tutorsData.filter((tutor) => tutor.id !== id));
+  };
+
   const renderTutorPapers = () =>
     tutorsData.map((tutor) => {
-      const id = nanoid();
       return (
-        <Paper key={id}>
+        <Paper key={tutor.id}>
           <div className={styles.tutorsContainer}>
             <div className={styles.tutorName}>
               <p>{tutor.firstName}</p>
-              <p>{tutor.LastName}</p>
+              <p>{tutor.lastName}</p>
             </div>
             <div className={styles.tutorContact}>
               <p>{tutor.phone}</p>
@@ -24,57 +28,42 @@ const Tutors = ({ tutors }) => {
               <p>{tutor.city}</p>
             </div>
             <p>{tutor.options}</p>
+
+            <ThemedButton onClick={() => handleRemoveTutor(tutor.id)}>
+              Remove Tutor
+            </ThemedButton>
           </div>
         </Paper>
       );
     });
 
-  const handleFormSubmit = (abstractData) =>
-    setTutorsData({ ...tutorsData, abstractData });
-
-  const renderAddNewTutorForm = () => (
-    <form>
-      <input
-        type="text"
-        name="firstName"
-        id="firstname"
-        placeholder="First Name"
-      />
-      <input
-        type="type"
-        name="lastName"
-        id="lastName"
-        placeholder="Last Name"
-      />
-      <input
-        type="tel"
-        name="phone"
-        id="phone"
-        placeholder="Phone Number"
-        pattern="0-9]{3}-[0-9]{3}-[0-9]{4}"
-        required
-      />
-      <input type="email" name="email" id="email" placeholder="Email" />
-      <input type="text" name="city" id="city" placeholder="City" />
-      <button onClick={handleFormSubmit}>Invite</button>
-    </form>
-  );
-  const handleAddtutorButton = setShowForm;
+  const handleFormSubmit = (newTutorData) => {
+    setTutorsData([...tutorsData, newTutorData]);
+  };
 
   return (
-    <div className={styles.wrapper}>
-      {renderTutorPapers()}
+    <>
+      <div className={styles.wrapper}>{renderTutorPapers()}</div>
       {showForm ? (
-        renderAddNewTutorForm()
+        <FormTutors updateParentState={handleFormSubmit} />
       ) : (
-        <button onClick={() => setShowForm(true)}>Add Tutor</button>
+        <ThemedButton onClick={() => setShowForm(true)}>Add Tutor</ThemedButton>
       )}
-    </div>
+    </>
   );
 };
 
 Tutors.propTypes = {
-  tutors: PropTypes.arrayOf(PropTypes.object),
+  tutors: PropTypes.arrayOf(
+    PropTypes.shape({
+      firstName: PropTypes.string.isRequired,
+      lastName: PropTypes.string.isRequired,
+      phone: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      city: PropTypes.string.isRequired,
+      options: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Tutors;
---
+++ react-app/src/helpers/themeColors.js
@@ -0,0 +1,14 @@
+export default {
+    light: {
+      primaryButtonBackground: "#FF6B0A",
+      primaryButtonColor: "#FFF",
+      paperBackground: "#fff",
+      paperColor: "#000",
+    },
+    dark: {
+      primaryButtonBackground: "#481E14",
+      primaryButtonColor: "#FFF",
+      paperBackground: "#242424",
+      paperColor: "#fff",
+    },
+}
